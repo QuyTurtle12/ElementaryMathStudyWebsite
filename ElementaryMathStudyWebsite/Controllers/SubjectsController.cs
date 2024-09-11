0dce0f9fa8394db91f127b1cfff637dc8db6196c
Baoaxid <thaitieubao2004@gmail.com>
2024-09-11 15:17:58 +0700

    complete crud subject (mini validation)

--- ElementaryMathStudyWebsite/Controllers/SubjectsController.cs
+++ ElementaryMathStudyWebsite/Controllers/SubjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ElementaryMathStudyWebsite.Contract.Services.Interface;
+using ElementaryMathStudyWebsite.Repositories.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElementaryMathStudyWebsite.Controllers
@@ -7,5 +8,104 @@ namespace ElementaryMathStudyWebsite.Controllers
     [ApiController]
     public class SubjectsController : ControllerBase
     {
+        private readonly ISubjectService _subjectService;
+
+        public SubjectsController(ISubjectService subjectService)
+        {
+            _subjectService = subjectService;
+        }
+
+        // GET: api/Subjects
+        [HttpGet]
+        public async Task<IActionResult> GetAllSubjects()
+        {
+            var subjects = await _subjectService.GetAllSubjectsAsync();
+
+            return Ok(subjects);
+        }
+
+        // GET: api/Subjects/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSubjectById(string id)
+        {
+            try
+            {
+                var subject = await _subjectService.GetSubjectByIDAsync(id);
+
+                var subjectDTO = new SubjectDTO
+                {
+                    SubjectName = subject.SubjectName,
+                    Price = subject.Price,
+                    Status = subject.Status
+                };
+
+                return Ok(subjectDTO);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+        // POST: api/Subjects
+        [HttpPost]
+        public async Task<IActionResult> CreateSubject([FromBody] CreateSubjectDTO subjectDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var createdSubjectDTO = new SubjectDTO
+            {
+                SubjectName = subjectDTO.SubjectName,
+                Price = subjectDTO.Price,
+                Status = true
+            };
+            var subject = await _subjectService.CreateSubjectAsync(createdSubjectDTO);
+
+
+            return CreatedAtAction(nameof(GetSubjectById), new { id = subject.Id }, createdSubjectDTO);
+        }
+
+        // PUT: api/Subjects/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSubject(string id, [FromBody] SubjectDTO subjectDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _subjectService.UpdateSubjectAsync(id, subjectDTO);
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
+
+        // PUT: api/Subjects/ChangeStatus/{id}
+        [HttpPut("/ChangeStatus/{id}")]
+        public async Task<IActionResult> ChangeSubjectStatus(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _subjectService.ChangeSubjectStatusAsync(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(new { message = ex.Message });
+            }
+        }
     }
 }
--- ElementaryMathStudyWebsite/Program.cs
+++ ElementaryMathStudyWebsite/Program.cs
@@ -20,6 +20,8 @@ namespace ElementaryMathStudyWebsite
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddConfig(builder.Configuration);
+            builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+              policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
