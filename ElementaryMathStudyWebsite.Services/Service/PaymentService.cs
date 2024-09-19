165915eddfe480ed5eb997ec8a5a4571487c8f21
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2024-09-19 12:12:05 +0700

    bug?

--- ElementaryMathStudyWebsite.Services/Service/PaymentService.cs
+++ ElementaryMathStudyWebsite.Services/Service/PaymentService.cs
@@ -29,112 +29,112 @@ namespace ElementaryMathStudyWebsite.Services.Service
             throw new NotImplementedException();
         }
 
-            var order = await _unitOfWork.GetRepository<Order>().GetByIdAsync(orderId);
+        var order = await _unitOfWork.GetRepository<Order>().GetByIdAsync(orderId);
 
-            Payment payment = new()
-            {
-                Id = Guid.NewGuid().ToString().ToUpper(),
-                CustomerId = order.CustomerId,
-                OrderId = orderId,
-                PaymentDate = order.CreatedTime,
-                PaymentMethod = "VnPay",
-                Amount = order.TotalPrice,
-                Status = "Success"
-            };
+        Payment payment = new()
+        {
+            Id = Guid.NewGuid().ToString().ToUpper(),
+            CustomerId = order.CustomerId,
+            OrderId = orderId,
+            PaymentDate = order.CreatedTime,
+            PaymentMethod = "VnPay",
+            Amount = order.TotalPrice,
+            Status = "Success"
+        };
 
-            await _unitOfWork.GetRepository<Payment>().InsertAsync(payment);
-            await _unitOfWork.SaveAsync();
+        await _unitOfWork.GetRepository<Payment>().InsertAsync(payment);
+        await _unitOfWork.SaveAsync();
 
-            PaymentViewDto paymentViewDto = new()
-            {
-                PaymentId = payment.Id,
-                CustomerName = order.User.FullName,
-                Amount = payment.Amount,
-                PaymentDate = payment.PaymentDate,
-                PaymentMethod = payment.PaymentMethod,
-                Status = payment.Status
-            };
+        PaymentViewDto paymentViewDto = new()
+        {
+            PaymentId = payment.Id,
+            CustomerName = order.User.FullName,
+            Amount = payment.Amount,
+            PaymentDate = payment.PaymentDate,
+            PaymentMethod = payment.PaymentMethod,
+            Status = payment.Status
+        };
 
             foreach (var subjectStudent in orderCreateDto.SubjectStudents)
             {
                 var subjectName = (await _unitOfWork.GetRepository<Subject>().GetByIdAsync(subjectStudent.SubjectId)).SubjectName;
-                var studentName = (await _unitOfWork.GetRepository<User>().GetByIdAsync(subjectStudent.StudentId)).FullName;
-                PaymentSubjectStudent paymentSubjectStudent = new()
-                {
-                    SubjectName = subjectName,
-                    StudentName = studentName
-                };
-                paymentViewDto.SubjectStudents.Add(paymentSubjectStudent);
+        var studentName = (await _unitOfWork.GetRepository<User>().GetByIdAsync(subjectStudent.StudentId)).FullName;
+        PaymentSubjectStudent paymentSubjectStudent = new()
+        {
+            SubjectName = subjectName,
+            StudentName = studentName
+        };
+        paymentViewDto.SubjectStudents.Add(paymentSubjectStudent);
             }
 
             return paymentViewDto;
         }
 
-        public async Task<Payment> GetPaymentById(string paymentId)
+public async Task<Payment> GetPaymentById(string paymentId)
+{
+    var payment = await _unitOfWork.GetRepository<Payment>().GetByIdAsync(paymentId) ?? throw new KeyNotFoundException("Invalid payment ID");
+    return payment;
+}
+
+public async Task<BasePaginatedList<PaymentViewDto>> GetPaymentHistory(int pageNumber, int pageSize)
+{
+    var appUserService = _userService as IAppUserServices;
+    string customerId = appUserService.GetActionUserId();
+
+    IQueryable<Payment> query = _unitOfWork.GetRepository<Payment>().Entities.Where(q => q.CustomerId == customerId);
+    List<PaymentViewDto> paymentViewDtos = new();
+
+    //If params negative = show all
+    if (pageNumber <= 0 || pageSize <= 0)
+    {
+        var allPayments = await query.ToListAsync();
+
+        foreach (var payment in allPayments)
         {
-            var payment = await _unitOfWork.GetRepository<Payment>().GetByIdAsync(paymentId) ?? throw new KeyNotFoundException("Invalid payment ID");
-            return payment;
+            paymentViewDtos.Add(PaymentViewMapper(payment));
         }
+        return new BasePaginatedList<PaymentViewDto>(paymentViewDtos, paymentViewDtos.Count, 1, paymentViewDtos.Count);
+    }
 
-        public async Task<BasePaginatedList<PaymentViewDto>> GetPaymentHistory(int pageNumber, int pageSize)
-        {
-            var appUserService = _userService as IAppUserServices;
-            string customerId = appUserService.GetActionUserId();
+    // Show with pagination
+    BasePaginatedList<Payment>? paginatedPayments = await _unitOfWork.GetRepository<Payment>().GetPagging(query, pageNumber, pageSize);
 
-            IQueryable<Payment> query = _unitOfWork.GetRepository<Payment>().Entities.Where(q => q.CustomerId == customerId);
-            List<PaymentViewDto> paymentViewDtos = new();
+    foreach (var payment in paginatedPayments.Items)
+    {
+        paymentViewDtos.Add(PaymentViewMapper(payment));
+    }
 
-            //If params negative = show all
-            if (pageNumber <= 0 || pageSize <= 0)
-            {
-                var allPayments = await query.ToListAsync();
+    return new BasePaginatedList<PaymentViewDto>(paymentViewDtos, paginatedPayments.TotalItems, pageNumber, pageSize);
+}
 
-                foreach (var payment in allPayments)
-                {
-                    paymentViewDtos.Add(PaymentViewMapper(payment));
-                }
-                return new BasePaginatedList<PaymentViewDto>(paymentViewDtos, paymentViewDtos.Count, 1, paymentViewDtos.Count);
-            }
+public async Task<BasePaginatedList<Payment>> GetPayments(int pageNumber, int pageSize)
+{
+    IQueryable<Payment> query = _unitOfWork.GetRepository<Payment>().Entities;
 
-            // Show with pagination
-            BasePaginatedList<Payment>? paginatedPayments = await _unitOfWork.GetRepository<Payment>().GetPagging(query, pageNumber, pageSize);
+    // Negative params = show all 
+    if (pageNumber <= 0 || pageSize <= 0)
+    {
+        List<Payment> allPayments = query.ToList();
+        return new BasePaginatedList<Payment>(allPayments, allPayments.Count, 1, allPayments.Count);
+    }
 
-            foreach (var payment in paginatedPayments.Items)
-            {
-                paymentViewDtos.Add(PaymentViewMapper(payment));
-            }
+    // Show with pagination
+    return await _unitOfWork.GetRepository<Payment>().GetPagging(query, pageNumber, pageSize);
 
-            return new BasePaginatedList<PaymentViewDto>(paymentViewDtos, paginatedPayments.TotalItems, pageNumber, pageSize);
-        }
+}
 
-        public async Task<BasePaginatedList<Payment>> GetPayments(int pageNumber, int pageSize)
-        {
-            IQueryable<Payment> query = _unitOfWork.GetRepository<Payment>().Entities;
+private PaymentViewDto PaymentViewMapper(Payment payment)
+{
+    return new PaymentViewDto
+    {
+        PaymentId = payment.Id,
+        CustomerName = payment.User.FullName,
+        PaymentDate = payment.PaymentDate,
+        Amount = payment.Amount,
+        PaymentMethod = payment.PaymentMethod,
+        Status = payment.Status
+    };
 
-            // Negative params = show all 
-            if (pageNumber <= 0 || pageSize <= 0)
-            {
-                List<Payment> allPayments = query.ToList();
-                return new BasePaginatedList<Payment>(allPayments, allPayments.Count, 1, allPayments.Count);
-            }
-
-            // Show with pagination
-            return await _unitOfWork.GetRepository<Payment>().GetPagging(query, pageNumber, pageSize);
-
-        }
-
-        private PaymentViewDto PaymentViewMapper(Payment payment)
-        {
-            return new PaymentViewDto
-            {
-                PaymentId = payment.Id,
-                CustomerName = payment.User.FullName,
-                PaymentDate = payment.PaymentDate,
-                Amount = payment.Amount,
-                PaymentMethod = payment.PaymentMethod,
-                Status = payment.Status
-            };
-
-        }
+}
     }
 }
