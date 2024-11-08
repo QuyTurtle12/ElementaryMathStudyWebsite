11fcce725afa6467f7c645b368025b244f208a2d
charleshuy <pathofdoomz@gmail.com>
2024-11-08 09:03:06 +0700

    User CRUD Razor Pages

---
+++ ElementaryMathStudyWebsite.RazorPage/Pages/UserPages/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElementaryMathStudyWebsite.Core.Repositories.Entity;
+using ElementaryMathStudyWebsite.Infrastructure.Context;
+
+namespace ElementaryMathStudyWebsite.RazorPage.Pages.UserPages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext _context;
+
+        public DeleteModel(ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.User.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ElementaryMathStudyWebsite.RazorPage/Pages/UserPages/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElementaryMathStudyWebsite.Core.Repositories.Entity;
+using ElementaryMathStudyWebsite.Infrastructure.Context;
+
+namespace ElementaryMathStudyWebsite.RazorPage.Pages.UserPages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext _context;
+
+        public DetailsModel(ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public new User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+    }
+}
