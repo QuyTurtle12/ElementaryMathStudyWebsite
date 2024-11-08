b622df9c8a89735be929182d5e31ef333b9cb112
dotuankiet2302 <dotuankiet2302@gmail.com>
2024-11-08 09:47:23 +0700

    updateRazorPages

---
+++ ElementaryMathStudyWebsite.RazorPage/Pages/ChapterPages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElementaryMathStudyWebsite.Core.Repositories.Entity;
+using ElementaryMathStudyWebsite.Infrastructure.Context;
+
+namespace ElementaryMathStudyWebsite.RazorPage.Pages.ChapterPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext _context;
+
+        public IndexModel(ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Chapter> Chapter { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Chapter = await _context.Chapter
+                .Include(c => c.Quiz)
+                .Include(c => c.Subject).ToListAsync();
+        }
+    }
+}
