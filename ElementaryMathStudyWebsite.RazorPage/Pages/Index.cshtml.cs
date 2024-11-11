8f47b22a63539429d31c75f74b79829e2fa165df
charleshuy <pathofdoomz@gmail.com>
2024-11-11 11:36:05 +0700

    Login page + session

--- ElementaryMathStudyWebsite.RazorPage/Pages/Index.cshtml.cs
+++ ElementaryMathStudyWebsite.RazorPage/Pages/Index.cshtml.cs
@@ -14,7 +14,22 @@ namespace ElementaryMathStudyWebsite.RazorPage.Pages
 
         public void OnGet()
         {
+        }
 
+        public JsonResult OnGetSessionInfo()
+        {
+            var userId = HttpContext.Session.GetString("user_id");
+            var roleId = HttpContext.Session.GetString("role_id");
+
+            // Return session info as JSON
+            return new JsonResult(new { userId, roleId });
+        }
+
+        public IActionResult OnPostLogout()
+        {
+            // Clear session data
+            HttpContext.Session.Clear();
+            return RedirectToPage("AuthPages/Login"); // Redirect to login page
         }
     }
 }
