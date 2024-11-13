2c7b79d75f875606bdb993b7aa4373695efcc782
unknown <tgq.2053@gmail.com>
2024-11-13 08:55:31 +0700

    Topic Pages

---
+++ ElementaryMathStudyWebsite.RazorPage/Pages/TopicPages/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElementaryMathStudyWebsite.Core.Repositories.Entity;
+using ElementaryMathStudyWebsite.Infrastructure.Context;
+
+namespace ElementaryMathStudyWebsite.RazorPage.Pages.TopicPages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext _context;
+
+        public DetailsModel(ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Topic Topic { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topic.FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Topic = topic;
+            }
+            return Page();
+        }
+    }
+}
