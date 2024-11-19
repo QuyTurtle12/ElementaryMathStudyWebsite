5aed0c930675e7de3d62b4d26e33ac039cd60382
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2024-11-19 15:02:46 +0700

    change index page of user answer to more user friendly

--- ElementaryMathStudyWebsite.RazorPage/Pages/UserAnswerPages/Index.cshtml.cs
+++ ElementaryMathStudyWebsite.RazorPage/Pages/UserAnswerPages/Index.cshtml.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ElementaryMathStudyWebsite.Core.Entity;
 using ElementaryMathStudyWebsite.Infrastructure.Context;
-using Microsoft.AspNetCore.Authorization;
+using ElementaryMathStudyWebsite.Contract.UseCases.DTOs.UserAnswerDtos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ElementaryMathStudyWebsite.RazorPage.Pages.UserAnswerPages
 {
-    [Authorize(Policy = "Admin-Content")] //authorize for admin and content manager
     public class IndexModel : PageModel
     {
         private readonly ElementaryMathStudyWebsite.Infrastructure.Context.DatabaseContext _context;
@@ -21,14 +19,39 @@ namespace ElementaryMathStudyWebsite.RazorPage.Pages.UserAnswerPages
             _context = context;
         }
 
-        public IList<UserAnswer> UserAnswer { get;set; } = default!;
+        public List<UserAnswerWithDetailsDTO> UserAnswers { get; set; } = new();
+        public string UserRole { get; private set; } = string.Empty;
+        public bool IsLoggedIn { get; private set; } = false;
+        public string UserId { get; private set; } = string.Empty;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            UserAnswer = await _context.UserAnswer
-                .Include(u => u.Option)
-                .Include(u => u.Question)
-                .Include(u => u.User).ToListAsync();
+            // Retrieve session information
+            UserId = HttpContext.Session.GetString("user_id");
+            UserRole = HttpContext.Session.GetString("role_name");
+
+            IsLoggedIn = true;
+
+            IQueryable<UserAnswer> query = _context.UserAnswer
+                .Include(u => u.Question) // Include related Question entity
+                .Include(u => u.Option)   // Include related Option entity
+                .Include(u => u.User);    // Include related User entity
+
+            // Fetch all user answers and map to DTO
+            UserAnswers = await query
+                .Select(u => new UserAnswerWithDetailsDTO
+                {
+                    QuestionId = u.QuestionId,
+                    QuestionContent = u.Question.QuestionContext,  // Map QuestionContent
+                    UserId = u.UserId,
+                    UserFullName = u.User.FullName,       // Map UserFullName
+                    OptionId = u.OptionId,
+                    OptionAnswer = u.Option.Answer,      // Map OptionAnswer
+                    AttemptNumber = u.AttemptNumber
+                })
+                .ToListAsync();
+
+            return Page();
         }
     }
 }
