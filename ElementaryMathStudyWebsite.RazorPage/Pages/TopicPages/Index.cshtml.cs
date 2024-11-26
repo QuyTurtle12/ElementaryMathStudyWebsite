cc3226decb2930b11778fc4aafa16fc125405069
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2024-11-27 02:06:39 +0700

    exam for topic complete

--- ElementaryMathStudyWebsite.RazorPage/Pages/TopicPages/Index.cshtml.cs
+++ ElementaryMathStudyWebsite.RazorPage/Pages/TopicPages/Index.cshtml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ElementaryMathStudyWebsite.Core.Repositories.Entity;
-using ElementaryMathStudyWebsite.Infrastructure.Context;
+using ElementaryMathStudyWebsite.Contract.UseCases.DTOs;
 
 namespace ElementaryMathStudyWebsite.RazorPage.Pages.TopicPages
 {
@@ -19,13 +15,52 @@ namespace ElementaryMathStudyWebsite.RazorPage.Pages.TopicPages
             _context = context;
         }
 
-        public IList<Topic> Topic { get;set; } = default!;
+        public List<TopicViewDto> Topic { get; set; } = new();
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchKeyword { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
+
+        public int TotalPages { get; private set; }
+        public int CurrentPage { get; private set; }
+
+        private const int PageSize = 10;
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            Topic = await _context.Topic
-                .Include(t => t.Chapter)
-                .Include(t => t.Quiz).ToListAsync();
+            IQueryable<Topic> query = _context.Topic
+                .Include(u => u.Chapter);
+
+            if (!string.IsNullOrEmpty(SearchKeyword))
+            {
+                query = query.Where(u => EF.Functions.Like(u.TopicName, $"%{SearchKeyword}%"));
+            }
+
+            // Count total items for pagination
+            int totalItems = await query.CountAsync();
+
+            // Calculate total pages
+            TotalPages = (int)Math.Ceiling(totalItems / (double)PageSize);
+            CurrentPage = PageNumber;
+
+            // Fetch paginated results
+            Topic = await query
+               .Skip((PageNumber - 1) * PageSize)
+               .Take(PageSize)
+               .Select(u => new TopicViewDto
+               {
+                   Id = u.Id,
+                   Number = u.Number,
+                   QuizId = u.QuizId,
+                   TopicName = u.TopicName,
+                   ChapterId = u.ChapterId,
+                   ChapterName = u.Chapter!.ChapterName
+               })
+               .ToListAsync();
+
+            return Page();
         }
     }
 }
